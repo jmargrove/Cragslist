c435a2c38faeec23f47636b310007480de28f128
KarinaDavtyan <k.davtyan27@gmail.com>
2017-09-22 18:43:43 +0200

    initial commit

---
+++ climbing-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// const muiTheme = getMuiTheme({
+//   fontFamily: 'Oswald',
+//   palette:{
+//     pickerHeaderColor:"#C0CA33",
+//     primary1Color: "#C0CA33",
+//     primary2Color: "#C0CA33",
+//   }
+// })
+
+class App extends Component {
+  render() {
+    return (
+    <h1>Hello</h1>
+    );
+  }
+}
+
+export default App;
