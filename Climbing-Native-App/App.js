1135cec08194fe0d8e936a22abcc3df4984cd21d
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2017-11-16 16:52:52 +0100

    feat: very basic login page

--- Climbing-Native-App/App.js
+++ Climbing-Native-App/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SignInComponent from './containers/SignInComponent.js'
+import SignInComponent from './src/containers/SignInComponent.js'
+import { COLOR, ThemeProvider, Button } from 'react-native-material-ui';
+
+const uiTheme = {
+    palette: {
+        primaryColor: "black",
+    },
+    toolbar: {
+        container: {
+            height: 50,
+        },
+    },
+    button: {
+    },
+};
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <SignInComponent/>
+      <ThemeProvider uiTheme={uiTheme}>
+        <SignInComponent/>
+      </ThemeProvider>
     );
   }
 }
--- Climbing-Native-App/src/containers/SignInComponent.js
+++ Climbing-Native-App/src/containers/SignInComponent.js
@@ -1,20 +1,32 @@
-// import React from 'react';
-// import RaisedButton from 'material-ui/RaisedButton';
-// import { Link } from 'react-router-dom';
-//
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+// import { RaisedButton } from 'react-native-material-ui'
+import { Navigator, NativeModules } from 'react-native';
+import { Button } from 'react-native-material-ui';
+
+
+
 
 
 class SignInComponent extends React.Component {
 
   render () {
     return (
-      <View style={styles.containers}>
-        {/* <Link to={`/sign-in`}>
-          <RaisedButton label="My profile" primary={true}  />
-        </Link> <br />
-        <Link to={`/create-user`}>
-          <RaisedButton label="Create profile" primary={true}  />
-        </Link> */}
+      <View style={styles.container}>
+        {/* <Image style={styles.backgroundImage} source={{uri: "https://s3-eu-west-1.amazonaws.com/climbing-kd/custom/rocks.png"}}/> */}
+        <View style={styles.boxes}>
+          <View style={styles.TitleBox}>
+            <Text style={styles.Title}>log-in</Text>
+          </View>
+        </View>
+        <View style={styles.boxes}>
+          <TextInput style={styles.inputBox}/>
+          <TextInput style={styles.inputBox}/>
+        </View>
+        <View style={styles.boxes}>
+          <Button raised primary text="log-in"/>
+          <Button raised primary text="Create account"/>
+        </View>
       </View>
     )
   }
@@ -23,11 +35,31 @@ class SignInComponent extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'orange',
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  boxes: {
+    backgroundColor: 'white',
+    width: 300,
+    height: 100,
+    borderColor: 'black',
+    justifyContent: 'space-around',
+  },
+  TitleBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Title: {
+    fontSize: 50,
+  },
+  inputBox: {
+    borderWidth: 1,
+    borderColor: 'black',
+  }
 });
 
 export default SignInComponent;
